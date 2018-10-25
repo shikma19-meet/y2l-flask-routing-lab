0862774f39b89ab64beb87ca1a019c05dd5187dc
aboud20-meet <aboud20@meet.mit.edu>
2018-10-25 18:50:10 +0300

    lab3

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return "You are home!"
+    return render_template("home.html")
+
+@app.route('/shop')
+def shop_page():
+	products = ["detoxifying charcoal mud mask","primRose oil mud mask","calming paper mask"]
+	return render_template("shop.html",products = products )
 
 if __name__ == '__main__':
    app.run(debug = True)
